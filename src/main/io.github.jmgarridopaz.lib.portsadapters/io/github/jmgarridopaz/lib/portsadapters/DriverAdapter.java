f036fccb96d95d163d3ed66e7a025255f1f5f5df
Juan <juanm.garrido.paz@gmail.com>
2021-10-05 01:51:13 +0200

    init

--- src/main/io.github.jmgarridopaz.lib.portsadapters/io/github/jmgarridopaz/lib/portsadapters/DriverAdapter.java
+++ src/main/io.github.jmgarridopaz.lib.portsadapters/io/github/jmgarridopaz/lib/portsadapters/DriverAdapter.java
@@ -1,19 +1,8 @@
 package io.github.jmgarridopaz.lib.portsadapters;
 
 
-public abstract class DriverAdapter<T> {
+public interface DriverAdapter {
 
-	private final T driverPort;
-	
-	protected DriverAdapter ( T driverPort ) {
-		this.driverPort = driverPort;
-	}
-
-	
-	protected T driverPort() {
-		return this.driverPort;
-	}
-
-	public abstract void run ( String[] args );
+	public void run ( String[] args );
 
 }
